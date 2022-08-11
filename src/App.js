798fe2b4e8a4ae3107c20d517c7fad6933dc3f23
Victoria Diaz <diazmariavictoria94@gmail.com>
2022-08-10 21:50:56 -0300

    agrego primera version de buscador de imagenes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { Formik, Form, Field } from "formik";
+import { useState } from "react";
+import "../src/styles/Header.css";
+import "../src/styles/Content.css";
+import "../src/styles/Article.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [photos, setPhotos] = useState([]);
+	const open = (url) => {
+		window.open(url);
+	};
+
+	return (
+		<div>
+			<header>
+				<Formik
+					initialValues={{ search: "" }}
+					onSubmit={async (values) => {
+						const response = await fetch(
+							`https://api.unsplash.com/search/photos?per_page=20&query=${values.search}`,
+							{
+								headers: {
+									Authorization:
+										"Client-ID dUIz-SX5-gwwzJ4GMLsjzCYX5nTH88rGHvruyP_C83U",
+								},
+							}
+						);
+						const data = await response.json();
+
+						setPhotos(data.results);
+					}}
+				>
+					<Form>
+						<label>Busca imagenes:</label>
+						<Field name="search" />
+					</Form>
+				</Formik>
+			</header>
+			<div className="container">
+				<div className="center">
+					{photos.map((photo) => {
+						return (
+							<article key={photo.id}>
+								<img
+									src={photo.urls.regular}
+									alt="Fotos"
+									onClick={() => open(photo.links.html)}
+								/>
+								<p>{[photo.description, photo.alt_description].join(" - ")}</p>
+							</article>
+						);
+					})}
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default App;
